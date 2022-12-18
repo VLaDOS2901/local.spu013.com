14ee21533d87977747bdcad2d03eeaa429b07566
Vlad Revutskyi <vlad.rewutsky01@gmail.com>
2022-12-18 22:49:21 +0200

    Lesson 2

--- add_product.php
+++ add_product.php
@@ -1,34 +1,44 @@
 <?php
-if($_SERVER["REQUEST_METHOD"]=="POST")
-{
-    echo "POST REQUEST SERVER";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $name = $_POST['name'];
     $price = $_POST['price'];
     $description = $_POST['description'];
-    $image = "";
-    $dir_save='images/';
-    $image_names = "";
-    for ($i=0; $i<count($_FILES["image"]["name"]); $i++)
-    {
-        $image .= " ";
-        $image .= $_FILES['image']['tmp_name'][$i];
-        $image_name =uniqid().'.jpg';
-        $image_names .= $image_name;
-        $image_names .= " ";
-        $uploadfile = $dir_save.$image_name;
-        move_uploaded_file($_FILES['image']['tmp_name'][$i], $uploadfile);
+
+
+    include_once($_SERVER['DOCUMENT_ROOT'] . '/options/connection_database.php');
+    $sql = 'INSERT INTO tbl_products (name, price, datecrate, description) VALUES (:name, :price, NOW(), :description);';
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':price', $price);
+    $stmt->bindParam(':description', $description);
+    $stmt->execute();
+
+    $sql = "SELECT LAST_INSERT_ID() as id;";
+    $item = $dbh->query($sql)->fetch();
+    $insert_id = $item['id'];
+
+    $images = $_POST['images'];
+    $count = 1;
+    foreach ($images as $base64) {
+        $dir_save = 'images/';
+        $image_name = uniqid() . '.jpeg';
+        $uploadfile = $dir_save . $image_name;
+        list(, $data) = explode(',', $base64);
+        $data = base64_decode($data);
+        file_put_contents($uploadfile, $data);
+        $sql = 'INSERT INTO tbl_product_images (name, datecreate, priority, product_id) VALUES(:name, NOW(), :priority, :product_id);';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':name', $image_name);
+        $stmt->bindParam(':priority', $count);
+        $stmt->bindParam(':product_id', $insert_id);
+        $stmt->execute();
+        $count++;
     }
 
-        include_once($_SERVER['DOCUMENT_ROOT'].'/options/connection_database.php');
-        $sql = 'INSERT INTO tbl_products (name, image, price, datecrate, description) VALUES (:name, :image, :price, NOW(), :description);';
-        $stmt=$dbh->prepare($sql);
-        $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':price', $price);
-        $stmt->bindParam(':description', $description);
-        $stmt->bindParam(':image', $image_names);
-        $stmt->execute();
-        header("Location: /");
-        exit();
+    header("Location: /");
+    exit();
+
 }
 ?>
 <!doctype html>
@@ -41,10 +51,12 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
     <title>Головна сторінка</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
 </head>
 <body>
-<?php include($_SERVER['DOCUMENT_ROOT'] . '/_header.php'); ?>
-
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/_header.php');
+?>
 <h1 class="text-center">Додати продукт</h1>
 <form class="col-md-6 offset-md-3" enctype="multipart/form-data" method="post">
     <div class="mb-3">
@@ -56,8 +68,21 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
         <input type="text" class="form-control" id="price" name="price">
     </div>
     <div class="mb-3">
-        <label for="image" class="form-label">Фото</label>
-        <input type="file" class="form-control" id="image" name="image[]" multiple>
+        <div class="container">
+            <div class="row" id="list_images">
+                <div class="col-md-3" id="selectImages">
+                    <label for="image" style="cursor: pointer" class="form-label text-success">
+                        <i class="fa fa-plus" style="font-size: 120px;" aria-hidden="true"></i>
+                    </label>
+
+                    <input type="file"
+                           class="form-control d-none"
+                           id="image"
+                           multiple>
+                </div>
+            </div>
+        </div>
+
     </div>
     <div class="mb-3">
         <label for="description" class="form-label">Опис</label>
@@ -66,6 +91,88 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
 
     <button type="submit" class="btn btn-primary">Додати</button>
 </form>
+
 <script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery-3.6.2.min.js"></script>
+<script>
+    //Генерація унікального Id
+    function uuidv4() {
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
+
+    $(function () {
+        const image = document.getElementById('image');
+        //Показати вибрані зображення
+        image.onchange = function (e) {
+            const files = e.target.files;
+
+            // console.log("Select files", files);
+            for (let i = 0; i < files.length; i++) {
+                // Оголосили reader
+                const reader = new FileReader();
+                //Підключили подію
+                reader.addEventListener('load', function () {
+                    const base64 = reader.result;
+                    // console.log(base64);
+                    const id = uuidv4();
+                    const data = `
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="fs-4 ms-2">
+                                    <label for="${id}">
+                                        <i class="fa fa-pencil" style="cursor: pointer;" aria-hidden="true"></i>
+                                    </label>
+                                    <input type="file" class="form-control d-none edit" id="${id}">
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="text-end fs-4 text-danger me-2 remove">
+                                    <i class="fa fa-times" style="cursor: pointer" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                <div>
+                    <img src="${base64}" id="${id}_image" width="100%">
+                    <input type="hidden" id="${id}_file" value="${base64}" name="images[]">
+                </div>
+                `;
+
+                    const item = document.createElement('div');
+                    item.className = 'col md-3 item-image';
+                    item.innerHTML = data;
+                    $("#selectImages").before(item);
+                });
+                const file = files[i];
+                // Передача файлу в reader
+                reader.readAsDataURL((file));
+            }
+            image.value = "";
+        }
+
+        //Видалити зображення
+        $("#list_images").on("click", ".remove", function () {
+            $(this).closest(".item-image").remove();
+        });
+
+        //Змінити зображення зі списку
+
+        let edit_id = 0;
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+            const base64 = reader.result;
+            document.getElementById(`${edit_id}_image`).src = base64;
+            document.getElementById(`${edit_id}_file`).value = base64;
+        });
+        $("#list_images").on("change", '.edit', function (e) {
+            edit_id = e.target.id;
+            const file = e.target.files[0];
+            reader.readAsDataURL(file);
+            this.value = "";
+        });
+
+    });
+</script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -23,78 +23,49 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/options/connection_database.php');
     <div class="container py-5">
         <div class="row">
             <?php
-            session_start();
+            $sql = "
+                Select p.id, p.name, p.price, pi.name as image 
+                from tbl_products as p, tbl_product_images as pi 
+                WHERE p.id=pi.product_id and pi.priority=1;
+                ";
 
-            $sql = "SELECT id,name,image,price FROM tbl_products;";
             if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST["delete"])) {
                 $del_id = $_POST["delete"];
                 include_once($_SERVER['DOCUMENT_ROOT'] . '/options/connection_database.php');
+                $sql1 = "DELETE FROM `tbl_product_images` WHERE product_id = '$del_id'";
                 $sql2 = "DELETE FROM tbl_products WHERE id = '$del_id'";
+                $dbh->query($sql1);
                 $dbh->query($sql2);
             }
 
-            if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST["details"])) {
-                $_SESSION['Id'] = $_POST["details"];
-                header("location: details.php");
-            }
-            foreach ($dbh->query($sql) as $row) :?>
-                <?php
-                $id = $row['id'];
-                $name = $row['name'];
-                $image = $row['image'];
-                $image = rtrim($image);
-                $images = explode(" ", $image);
-                $price = $row['price'];
-                ?>
-
-                <div class="col-md-6 col-lg-4 mb-4 mb-md-0 mt-3">
-                    <div class="card">
-                        <!--                    <div class="d-flex flex-wrap">-->
-
-                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-
-                            <div class="carousel-inner">
-                                <?php
-                                $counter = 0;
-                                foreach ($images as $img){
-                                if ($counter == 0) {
-                                    echo '<div class="carousel-item active">
-                                    <img src="images/'.$img.'" class="d-block w-100" alt="pizza">
-                                </div>';
-                                }
-                                else{
-                                    echo '<div class="carousel-item">
-                                    <img src="images/'.$img.'" class="d-block w-100" alt="pizza">
-                                </div>';
-                                }
-                                $counter++;
-                                }
-                                ?>
-                            </div>
-                        </div>
-                        <?php
-                        echo '<div class="card-body">
-
+            foreach ($dbh->query($sql) as $row)
+            {
+                $id=$row['id'];
+                $name=$row['name'];
+                $image=$row['image'];
+                $price=$row['price'];
+                echo '
+            <div class="col-md-6 col-lg-4 mb-4 mb-md-0 mt-3">
+                <div class="card">
+                    <img src="images/' . $image . '"
+                         class="card-img-top" alt="Gaming Laptop"/>
+                    <div class="card-body">
                         <div class="d-flex justify-content-between mb-3">
                             <h5 class="mb-0">' . $name . '</h5>
                             <h5 class="text-dark mb-0">' . $price . '</h5>
                         </div>
-
                         <div class="mb-2 text-end">
                         <form method="post">
-                            <button type="button" class="btn btn-success">Купити</button>
-                            <button type="submit" class="btn btn-success" name="details" value=' . $id . '>Детально</button>
+                            <a href="product.php?id=' . $id . '" class="btn btn-success">Купити</a>
                             <button type="submit" class="btn btn-danger" name="delete" value=' . $id . '>Видалити</button>
                         </form>
                         </div>
-                    </div>';
-                        ?>
-
                     </div>
                 </div>
-
-
-            <?php endforeach; ?>
+            </div>
+                    ';
+            }
+            ?>
         </div>
     </div>
 </section>
